e982facd0b81c4699e5c8b897d837cd868df6cec
d4funk <olivier.loustaunau@gmail.com>
2019-12-23 04:49:18 -0800

    Moving API module to apps folder

---
+++ grandpy/apps/api/gmaps.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+
+class GMaps:
+    """docstring for GMaps."""
+
+    def __init__(self):
+        super(GMaps, self).__init__()
+
+
+class GMapsAPI:
+    """docstring for GMapsAPI."""
+
+    def __init__(self):
+        super(GMapsAPI, self).__init__()
---
+++ grandpy/apps/api/wiki.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+
+class Wiki:
+    """docstring for Wiki."""
+
+    def __init__(self, page_id, title, extract, link):
+        super(Wiki, self).__init__()
+        self.page_id = page_id
+        self.title = title
+        self.extract = extract
+        self.link = link
+
+
+class WikiAPI:
+    """docstring for WikiAPI."""
+
+    def __init__(self):
+        super(WikiAPI, self).__init__()
