88882e4593046cc7089cb53a1c17f358d604c90b
Cosmopanda <olivier.loustaunau@gmail.com>
2020-02-25 09:56:07 -0800

    R9G

---
+++ grandpy/apps/gmaps/api.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+import os
+import json
+
+import requests
+
+from .models import Place
+
+GMAPS_URL = (
+    "https://maps.googleapis.com/maps/api/place/findplacefromtext/json?"
+)
+GMAPS_KEY = os.environ["GMAPS_KEY"]
+
+
+class GMapsAPI:
+    """GMaps API class
+    Instance of this class can call endpoints on GMaps API
+    To use this API, you need to put your Google API's key in your environment
+    If you are on Linux, type the following in your terminal:
+    export GMAPS_KEY=MY_API_KEY
+    """
+
+    def __init__(self):
+        super(GMapsAPI, self).__init__()
+
+    def place(self, search_term):
+        """Place endpoint
+        Args:
+            search_term: string
+        Returns:
+            name
+            geometry
+            formatted_address
+        """
+        query = f"input={search_term}&inputtype=textquery&fields=formatted_address,name,geometry"
+        try:
+            response = requests.get(f"{GMAPS_URL}{query}&key={GMAPS_KEY}")
+            if response.status_code == 200:
+                return response.json()
+        except requests.exceptions.RequestException as e:
+            print(e)
---
+++ grandpy/apps/wiki/api.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+import json
+
+import requests
+
+WIKI_URL = "https://fr.wikipedia.org/w/api.php?action=query&"
+
+
+class WikiAPI:
+    """docstring for WikiAPI."""
+
+    def __init__(self):
+        super(WikiAPI, self).__init__()
+
+    def geo(self, place):
+        query = (
+            f"list=geosearch&gscoord={place.latitude}|{place.longitude}"
+            f"&gsradius=10000&gslimit=10&format=json"
+        )
+        try:
+            response = requests.get(f"{WIKI_URL}{query}")
+            if response.status_code == 200:
+                return response.json()
+        except requests.exceptions.RequestException as e:
+            print(e)
+
+    def page(self, page):
+        query = (
+            f"pageids={page['pageid']}&prop=extracts&exintro&"
+            "format=json&prop=info|extracts&inprop=url"
+        )
+        try:
+            response = requests.get(f"{WIKI_URL}{query}")
+            if response.status_code == 200:
+                return response.json()
+        except requests.exceptions.RequestException as e:
+            print(e)
