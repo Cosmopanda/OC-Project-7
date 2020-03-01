3e46ded2730194d3074a1a6914dcb97cb315acc1
zetsumeishi <olivier.loustaunau@gmail.com>
2020-03-01 04:43:02 -0800

    Formatting answer

--- grandpy/app.py
+++ grandpy/app.py
@@ -7,16 +7,20 @@
 #        \/    \/          \/            \/     \/        \/     \/
 # Made on patorjk.com
 
+import os
 import json
 
 import nltk
 from flask_cors import CORS
 from flask import Flask, render_template, jsonify, request
 
-from grandpy.apps.query.forms import QueryForm
-from grandpy.apps.query.query import Query
-from grandpy.apps.gmaps.gmaps import GMapsAPI
-from grandpy.apps.wiki.wiki import WikiAPI
+from .constants import MESSAGES
+from .apps.query.forms import QueryForm
+from .apps.query.query import Query
+from .apps.gmaps.api import GMapsAPI
+from .apps.gmaps.models import Place
+from .apps.wiki.api import WikiAPI
+from .apps.wiki.models import Page
 
 app = Flask(__name__)
 CORS(app, resources={r"/query": {"origins": "*"}})
@@ -24,11 +28,43 @@ app.config.from_pyfile("settings.py")
 
 nltk.download("punkt")
 
+GMAPS_KEY = os.environ["GMAPS_KEY"]
 
-def format_answer(place, page):
-    answer = (
-        f"Adresse: {place.address}\nExtract: {page.extract}\nURL: {page.url}\n"
-    )
+
+def format_answer(place=None, page=None):
+    answer = ""
+
+    if not place:
+        answer += MESSAGES["not_found"]
+        return answer
+    if place:
+        if not page:
+            answer += MESSAGES["no_wiki_result"]
+            answer += f"Voici l'addresse pour {place.name}: {place.address}. "
+            answer += f"""
+            <iframe
+              width="400"
+              height="400"
+              frameborder="0" style="border:0"
+              src="https://www.google.com/maps/embed/v1/view?key={GMAPS_KEY}
+              &center={place.latitude},{place.longitude}
+              &zoom=17" allowfullscreen>
+            </iframe>
+            """
+        else:
+            answer += f"Voici l'addresse pour {place.name}: {place.address}. "
+            answer += MESSAGES["random"][0]
+            answer += f"{page.extract} Tu peux trouver plus d'informations ici <a href='{page.url}'>Wikipedia</a><br/>"
+            answer += f"""
+            <iframe
+              width="400"
+              height="400"
+              frameborder="0" style="border:0"
+              src="https://www.google.com/maps/embed/v1/view?key={GMAPS_KEY}
+              &center={place.latitude},{place.longitude}
+              &zoom=17" allowfullscreen>
+            </iframe>
+            """
     return answer
 
 
@@ -46,12 +82,21 @@ def query():
             if query:
                 parser = Query(query)
                 search_term = parser.parse()
-                gmaps = GMapsAPI(search_term)
-                place = gmaps.run()
+                gmaps = GMapsAPI()
+                response = gmaps.place(search_term)["candidates"][0]
+                place = Place(response)
                 if not place:
-                    return jsonify(query=query, answer="Je n'ai pas compris.")
+                    answer = format_answer()
+                    return jsonify(query=query, answer=answer)
                 wiki = WikiAPI()
-                page = wiki.run(place)
+                geo_search = wiki.geo(place)["query"]["geosearch"][0]
+                response = wiki.page(geo_search)["query"]["pages"][
+                    str(geo_search["pageid"])
+                ]
+                page = Page(response)
+                if not page:
+                    answer = format_answer(place=place)
+                    return jsonify(query=query, answer=answer)
                 answer = format_answer(place, page)
                 return jsonify(query=query, answer=answer)
         except Exception as e:
